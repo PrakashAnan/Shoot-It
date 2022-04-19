c022eaafa8c565bcffc78d03914c2c0ff61e590e
arman278012 <aa278012@gmail.com>
2022-04-20 01:55:20 +0530

    ....

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const app= express();
 const port=5000;
-const cors=require('cors');
+const cors=require('cors'); 
 
 
 const userRouter = require("./routers/userRouter");
+const equipmentRouter = require("./routers/equipmentRouter");
+
+app.use(express.json());
 
 app.use(cors({
     origin : ['http://localhost:3000']
 
 }))
 
-
-const equipmentRouter = require("./routers/equipmentRouter");
-
-
 app.use("/admin", userRouter);
 app.use("/equipment", equipmentRouter);
 
-app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send("you got a response")
+})
+
+app.listen(port,()=>{
+    console.log("my major server is started");
+})
+
+
 
 
 
