09775621171d5971d851a7e85818e3fdd5f99ecf
arman278012 <aa278012@gmail.com>
2022-05-25 16:44:33 +0530

    ...

--- frontend/src/components/main/home.js
+++ frontend/src/components/main/home.js
@@ -34,11 +34,102 @@ const Home = () => {
             </div>
           </section>
         </section>
-
       </div>
+
+
+      <div id="preview" class="preview">
+        <div style={{display: 'none'}}></div>
+        <div>
+          <div data-draggable="true" class="" style={{position: 'relative'}} draggable="false">
+
+
+            <section draggable="false" class="container" data-v-271253ee="">
+              <section class="mb-10">
+                <h2 class="fw-bold mb-5 text-center">Latest articles</h2>
+                <div class="row gx-lg-5 mb-5 align-items-center">
+                  <div class="col-md-6 mb-4 mb-md-0">
+                     <img src="https://mdbootstrap.com/img/new/standard/city/028.jpg"
+                    class="w-100 shadow-4-strong rounded-4 mb-4" alt="" aria-controls="#picker-editor" draggable="false"/>
+                  </div>
+                  <div class="col-md-6 mb-4 mb-md-0">
+                    <h3 class="fw-bold">Art</h3>
+                    <div class="mb-2 text-danger small"> <i class="fas fa-money-bill me-2"
+                      aria-controls="#picker-editor"></i><span>Business</span> </div>
+                    <p class="text-muted">Ut pretium ultricies dignissim. Sed sit amet mi eget urna placerat vulputate. Ut
+                      vulputate est non quam dignissim elementum. Donec a ullamcorper diam.</p>
+                    <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae nulla saepe rerum
+                      aspernatur odio amet perferendis tempora mollitia? Ratione unde magni omnis quaerat blanditiis cumque
+                      dolore placeat rem dignissimos?</p> <a class="btn  btn-secondary" href="#" role="button"
+                        aria-controls="#picker-editor" draggable="false">Read more</a>
+                  </div>
+                </div>
+                <div class="row gx-lg-5 mb-5 flex-lg-row-reverse align-items-center">
+                  <div class="col-md-6 mb-4 mb-md-0">
+                     <img src="https://mdbootstrap.com/img/new/standard/city/033.jpg"
+                    class="w-100 shadow-4-strong rounded-4 mb-4" alt="" aria-controls="#picker-editor" draggable="false"/>
+                  </div>
+                  <div class="col-md-6 mb-4 mb-md-0">
+                    <h3 class="fw-bold">Exhibition in Paris</h3>
+                    <div class="mb-2 text-primary small"> <i class="fas fa-money-bill me-2"
+                      aria-controls="#picker-editor"></i><span>Art</span> </div>
+                    <p class="text-muted">Duis sagittis, turpis in ullamcorper venenatis, ligula nibh porta dui, sit amet
+                      rutrum enim massa in ante. Curabitur in justo at lorem laoreet ultricies. Nunc ligula felis, sagittis
+                      eget nisi vitae, sodales vestibulum purus. Vestibulum nibh ipsum, rhoncus vel sagittis nec, placerat
+                      vel justo. Duis faucibus sapien eget tortor finibus, a eleifend lectus dictum. Cras tempor convallis
+                      magna id rhoncus. Suspendisse potenti. Nam mattis faucibus imperdiet. Proin tempor lorem at neque
+                      tempus aliquet. Phasellus at ex volutpat, varius arcu id, aliquam lectus. Vestibulum mattis felis quis
+                      ex pharetra luctus. Etiam luctus sagittis massa, sed iaculis est vehicula ut.</p> <a
+                        class="btn  btn-secondary" href="#" role="button" aria-controls="#picker-editor" draggable="false"
+                        >Read more</a>
+                  </div>
+                </div>
+                <div class="row gx-lg-5 mb-5 align-items-center">
+                  <div class="col-md-6 mb-4 mb-md-0">
+                     <img src="https://mdbootstrap.com/img/new/standard/city/079.jpg"
+                    class="w-100 shadow-4-strong rounded-4 mb-4" alt="" aria-controls="#picker-editor" draggable="false"/>
+                  </div>
+                  <div class="col-md-6 mb-4 mb-md-0">
+                    <h3 class="fw-bold">Stock market boom</h3>
+                    <div class="mb-2 text-warning small"> <i class="fas fa-money-bill me-2"
+                      aria-controls="#picker-editor"></i><span>Business</span> </div>
+                    <p class="text-muted">Sed sollicitudin purus sed nulla dignissim ullamcorper. Aenean tincidunt vulputate
+                      libero, nec imperdiet sapien pulvinar id. Nullam scelerisque odio vel lacus faucibus, tincidunt
+                      feugiat augue ornare. Proin ac dui vel lectus eleifend vestibulum et lobortis risus. Nullam in commodo
+                      sapien. Curabitur ut erat congue sem finibus eleifend egestas eu metus. Sed ut dolor id magna rutrum
+                      ultrices ut eget libero. Duis vel porttitor odio. Ut pulvinar sed turpis ornare tincidunt. Donec
+                      luctus, mi euismod dignissim malesuada, lacus lorem commodo leo, tristique blandit ante mi id metus.
+                      Integer et vehicula leo, vitae interdum lectus. Praesent nulla purus, commodo at euismod nec, blandit
+                      ultrices erat. Aliquam eros ipsum, interdum et mattis vitae, faucibus vitae justo. Nulla condimentum
+                      hendrerit leo, in feugiat ipsum condimentum ac. Maecenas sed blandit dolor.</p> <a
+                        class="btn  btn-secondary" href="#" role="button" aria-controls="#picker-editor" draggable="false"
+                        >Read more</a>
+                  </div>
+                </div>
+              </section>
+            </section>
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <script type="text/javascript" src="js/mdb.min.js"></script>
+
+      <script type="text/javascript"></script>
+
+
+
+
+
+
+
+
+
+
+
       <div data-draggable="true" className="" style={{ position: 'relative' }}>
-
-
         <section draggable="false" className="container-fluid " data-v-271253ee="">
           <section className="mb-10 text-center">
             <h2 className="fw-bold mb-7 text-center">Projects we are proud of</h2>
@@ -46,7 +137,7 @@ const Home = () => {
               <div className="col-lg-4 col-md-12 mb-6 mb-lg-0">
                 <div className="card shadow-2-strong">
                   <div className="bg-image hover-overlay ripple shadow-4-strong rounded mx-3" data-mdb-ripple-color="light"
-                    style={{ marginTop:'-15px' }}> <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/002.webp"
+                    style={{ marginTop: '-15px' }}> <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/002.webp"
                       className="img-fluid" alt="" aria-controls="#picker-editor" /> </div>
                   <div className="card-body">
                     <h5 className="card-title">White city</h5>
@@ -165,7 +256,7 @@ const Home = () => {
         </section>
 
       </div>
-    </div>
+    </div >
   )
 };
 
