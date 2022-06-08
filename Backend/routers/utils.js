bf7bf65cbbacb4ba389ac53ded1315d0335c2cfc
m-triple-m <triplem656@gmail.com>
2022-06-08 19:51:36 +0530

    ..

--- Backend/routers/utils.js
+++ Backend/routers/utils.js
@@ -1,20 +1,67 @@
-const multer =  require('multer')
-const router = require("express").Router()
+const multer = require("multer");
+const router = require("express").Router();
+const { SMTPClient } = require("emailjs");
 
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'./static/uploads')
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./static/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const myStorage = multer({ storage: storage });
+
+router.post("/uploadfile", myStorage.single("myfile"), (req, res) => {
+  res.status(200).json({ status: "success" });
+});
+
+const initMail = () => {
+  return new SMTPClient({
+    user: "startupmuneem3@gmail.com",
+    password: "rpzjbdmmvkzrmxxc",
+    host: "smtp.gmail.com",
+    ssl: true,
+  });
+};
+
+const client = initMail();
+const sendMail = (to, subject, text) => {
+  client.send(
+    {
+      text: text,
+      from: "startupmuneem3@gmail.com",
+      to: to,
+
+      cc: "",
+      subject: subject,
     },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
+    (err, message) => {
+      console.log(err || message);
     }
-    
-})
-const myStorage=multer({storage:storage})
+  );
+};
 
-router.post('/uploadfile',myStorage.single('myfile'),(req,res)=>{ 
-    res.status(200).json({status:'success'})
+router.post("/sendmail", (req, res) => {
+  const data = req.body;
+  sendMail(data.to, data.subject, data.text);
+  res.status(200).json({ message: "mail sent successfully" });
+});
 
-})
+router.post("/", (req, res) => {
+  console.log(req.body);
+
+  new Model(req.body)
+    .save()
+    .then((data) => {
+      console.log("Email Sent successfully..");
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
