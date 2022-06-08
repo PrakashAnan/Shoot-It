8a10c564adfefcd76158a6ec15ffbae54ff142bf
arman278012 <aa278012@gmail.com>
2022-06-08 19:57:47 +0530

    ..

--- Backend/connection.js
+++ Backend/connection.js
@@ -3,8 +3,8 @@ const mongoose=require("mongoose");
 const api_config=require("./config");
 
 const url=api_config.db_url;
-
 mongoose.connect(url)
+
 // for the connection
 
 
--- Backend/routers/orderRouter.js
+++ Backend/routers/orderRouter.js
@@ -3,7 +3,7 @@ const Model = require("../models/orderModel");
 
 
 router.post("/add", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
   
     new Model(req.body)
       .save()
@@ -15,20 +15,20 @@ router.post("/add", (req, res) => {
         console.error(err);
         res.status(500).json(err);
       });
-  });
+    });
+    router.get("/getall", (req, res) => {
+      Model.find({})
+        .then((data) => {
+          console.log("user list fetched successfully..");
+          res.status(200).json(data);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+    
 
-  router.get("/getall", (req, res) => {
-    Model.find({})
-      .then((data) => {
-        console.log("user list fetched successfully..");
-        res.status(200).json(data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
   router.delete("/delete/:id", (req, res) => {
     Model.findByIdAndDelete(req.params.id)
       .then((data) => {
